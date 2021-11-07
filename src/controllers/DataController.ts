b5bfbcd6d34e1ab5ec69df0a834f805596d16f82
Paulo <paulo_sanches2012@hotmail.com>
2021-11-06 22:58:10 -0300

    criação de rota de busca por id

--- src/controllers/DataController.ts
+++ src/controllers/DataController.ts
@@ -15,6 +15,7 @@ export function indexAll(request: Request, response: Response){
 
 export async function store(request: Request, response: Response): Promise<Response> {
     const {
+        id,
         featured,
         title,
         url,
@@ -27,6 +28,7 @@ export async function store(request: Request, response: Response): Promise<Respo
     } = request.body as Data;
 
     const data = await BaseData.create({
+        id,
         featured,
         title,
         url,
@@ -38,5 +40,19 @@ export async function store(request: Request, response: Response): Promise<Respo
         events
     });
 
-    return response.json(data)
+    return response.status(201).json(data)
+}
+
+export async function showByUser(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    
+    const idConvert = Number(id);
+    
+    const idArticle = await BaseData.find({ id: idConvert });
+
+    if (idArticle.length === 0) {
+        response.status(404).json({ message: "article not found!" });
+    }
+
+    return response.status(200).json(idArticle);
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,13 @@
 import { Router } from "express";
-import { index, indexAll, store } from "./controllers/DataController";
+import { index, indexAll, store, showByUser } from "./controllers/DataController";
 
 const routes = Router();
 
 routes.get("/", index)
 routes.get("/articles", indexAll)
+routes.get("/articles/:id", showByUser)
 
 routes.post("/articles", store)
-// routes.get("/articles", (request, response) => {
-//     api.get("/articles").then(res => {
-//         return response.status(200).json(res.data)
-//     })
-// })
 
 
 export { routes }
